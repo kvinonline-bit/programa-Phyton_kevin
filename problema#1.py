3830c545fa91c03a19a92b734bd1406fe13eb6d3
kvinonline-bit <kvinonline@gmail.com>
2026-05-23 22:17:05 -0500

    Add files via upload

--- problema#1.py
+++ problema#1.py
@@ -3,7 +3,19 @@
 #Programa: Fundamentos de programacion
 #Codigo Fuente: Autoria Propia 
 #ejercicio 1
-
+"""
+Problema 1: Una matriz almacena datos de sesiones de clientes con el 
+formato: [ID Cliente, Duración (segundos), Eventos Clics].  
+Se necesita una herramienta para evaluar el nivel de compromiso de 
+cada sesión.
+Requisitos de Desarrollo: - Datos Iniciales: Una matriz con al menos 5 filas de datos. 
+- Módulos: Se requiere un módulo (función) para calcular la clasificación de compromiso de una sesión basándose en su duración y clics. 
+- Lógica de Negocio: 
+✓ Clasificar como "Alto" (si Duración > 180s y Clics > 8).  
+✓ Clasificar como "Bajo" (si Duración < 60s o Clics < 3). 
+✓ Clasificar como "Medio" en todos los demás casos. 
+- Salida: Generar un informe listando el ID del cliente y su 
+clasificación final."""
 
 sesiones = [
     [101, 240, 12],  
